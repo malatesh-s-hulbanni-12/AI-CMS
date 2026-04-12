777380c2d969168ddaf24cd1036a14b8b426faa7
malatesh-s-hulbanni-12 <mshulbanni95@gmail.com>
2026-04-12 13:48:49 +0530

    Update allowedOrigins for CORS configuration

--- backend/server.js
+++ backend/server.js
@@ -15,8 +15,8 @@ const PORT = process.env.PORT || 5000;
 const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:5174",
-  "https://pdms-creater.vercel.app",
-  "https://pdms-admin.vercel.app",
+  "https://ai-cms-creaters.vercel.app",
+  "https://ai-cms-admin.vercel.app",
 ];
 
 app.use(
