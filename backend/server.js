131862e2a545251d46e81a8e08444f99366f7e06
malatesh-s-hulbanni-12 <mshulbanni95@gmail.com>
2026-04-12 13:58:03 +0530

    Update allowedOrigins for CORS configuration

--- backend/server.js
+++ backend/server.js
@@ -15,8 +15,8 @@ const PORT = process.env.PORT || 5000;
 const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:5174",
-  "https://ai-cms-creaters.vercel.app",
-  "https://ai-cms-admin.vercel.app",
+  "https://ai-cms-creaters.onrender.com",
+  "https://ai-cms-admin.onrender.com",
 ];
 
 app.use(
